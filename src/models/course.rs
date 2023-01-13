00a8df0d47ed04be0b38defe6a324a66e066a254
Miika Tuominen <miika.km.tuominen@gmail.com>
2023-01-13 20:21:56 +0200

    rename tto graph-api for consistency, fix README, add Cargo.toml

--- src/models/course.rs
+++ src/models/course.rs
@@ -1,7 +1,7 @@
-use juniper::{GraphQLObject};
-use serde::{Serialize, Deserialize};
-use diesel::{Queryable, Insertable};
 use crate::models::schema::courses;
+use diesel::{Insertable, Queryable};
+use juniper::GraphQLObject;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(GraphQLObject, Serialize, Deserialize, Queryable, Insertable, Debug)]
@@ -10,3 +10,16 @@ pub struct Course {
     pub created_at: chrono::DateTime<chrono::Utc>,
     pub name: String,
 }
+
+// IMPORTANT NOTE: the struct here represents both the GraphQL object and the PostgreSQL table
+
+// PostgreSQL:
+// The Queryable struct needs to be defined in the same way as it is in schema.rs,
+// and field/column order needs to also be the same.
+// See the following doc on how the types map: https://docs.rs/diesel/0.11.0/diesel/types/index.html.
+
+// GraphQL:
+// If we needed complex resolvers, we'd need to impl a #[graphql_object] manually, like we did for Query.
+// If you need the database, remember to add context = Context.
+// See the following issue for status about combining #[graphql_object] with #[derive(GraphQLObject)]:
+// https://github.com/graphql-rust/juniper/issues/553
