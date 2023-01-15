4ca4b7eedfcf53fb5ca21905e055ea0930554ca5
Miika Tuominen <miika.km.tuominen@gmail.com>
2023-01-15 14:54:59 +0200

    support units

--- src/models/course.rs
+++ src/models/course.rs
@@ -1,15 +1,48 @@
-use super::schema::courses;
+use super::schema::{courses, units};
+use crate::graph::context::Context;
+use diesel::BelongingToDsl;
+use diesel_async::RunQueryDsl;
 
-use diesel::{Insertable, Queryable};
-use juniper::{GraphQLInputObject, GraphQLObject};
+use diesel::{Associations, Identifiable, Insertable, Queryable};
+use juniper::{graphql_object, FieldResult, GraphQLInputObject, GraphQLObject};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(GraphQLObject, Serialize, Deserialize, Queryable, Debug)]
+#[derive(Serialize, Deserialize, Identifiable, Queryable, Debug)]
+#[diesel(table_name = courses)]
 pub struct Course {
     pub id: Uuid,
     pub created_at: chrono::DateTime<chrono::Utc>,
     pub name: String,
+}
+
+#[graphql_object(
+    context = Context
+)]
+impl Course {
+    pub fn id(&self) -> Uuid {
+        self.id
+    }
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+    pub fn created_at(&self) -> chrono::DateTime<chrono::Utc> {
+        self.created_at
+    }
+    pub async fn units(&self, context: &Context) -> FieldResult<Vec<Unit>> {
+        let conn = &mut context.diesel_pool.get().await?;
+        Ok(Unit::belonging_to(self).load::<Unit>(conn).await?)
+    }
+}
+
+#[derive(GraphQLObject, Serialize, Deserialize, Identifiable, Queryable, Debug, Associations)]
+#[belongs_to(Course)]
+pub struct Unit {
+    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub name: String,
+    pub id: Uuid,
+    pub parent_unit: Option<Uuid>,
+    pub course_id: Uuid,
 }
 
 #[derive(GraphQLInputObject, Serialize, Deserialize, Debug)]
