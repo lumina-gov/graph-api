182bc2ca168602d95d4cb1190cb18219683b82eb
Albert <albert@framework.tools>
2023-04-20 14:26:39 +0930

    Update script to not use transactions

--- migrations/2023-04-20-041610_delete_mongo_columns/down.sql
+++ migrations/2023-04-20-041610_delete_mongo_columns/down.sql
@@ -1,11 +1,6 @@
 -- This file should undo anything in `up.sql`
-
-BEGIN;
-
 ALTER TABLE users
 ADD COLUMN referrer_mongo varchar;
 
 ALTER TABLE users
 ADD COLUMN object_id varchar;
-
-COMMIT;
--- migrations/2023-04-20-041610_delete_mongo_columns/up.sql
+++ migrations/2023-04-20-041610_delete_mongo_columns/up.sql
@@ -1,11 +1,6 @@
 -- Your SQL goes here
-
-BEGIN;
-
 ALTER TABLE users
 DROP COLUMN referrer_mongo;
 
 ALTER TABLE users
 DROP COLUMN object_id;
-
-COMMIT;
