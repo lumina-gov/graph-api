69f91f075eb669e200c48be9daffa179e5d0514a
Albert <albert@framework.tools>
2023-05-03 21:05:15 +0930

    Added test for getting question_assessment

--- tests/question_assessment.rs
+++ tests/question_assessment.rs
@@ -7,28 +7,89 @@ async fn can_do_question_assessment() -> Result<(), anyhow::Error> {
     let email = shared::create_user().await?;
     let token = shared::login_specific(&email).await?;
 
-    let query = format!(
-        r#"
-        mutation {{
-            question_assessment(course_slug: "{}", unit_slug: "{}", question_slug: "{}", question: "{}", answer: "{}") {{
-                feedback
-                assessment
-            }}
-        }}
-    "#,
+    let response = create_question_assessment(
         "test-course",
         "test-unit",
         "test-question",
         "What is 1+1?",
         "2",
-    );
-
-    let response = shared::query(&query, &token).await?;
+        &token,
+    ).await?;
 
     assert_eq!(response["errors"], json!(null));
 
     assert!(response["data"]["question_assessment"]["feedback"].is_string());
     assert!(response["data"]["question_assessment"]["assessment"].is_string());
 
+    let response = get_question_assessment(
+        "test-course",
+        "test-unit",
+        "test-question",
+        &token,
+    ).await?;
+
+    assert_eq!(response["errors"], json!(null));
+
+    assert_eq!(response["data"]["question_assessment"]["answer"], "2");
+
     Ok(())
+}
+
+
+async fn create_question_assessment(
+    course_slug: &str,
+    unit_slug: &str,
+    question_slug: &str,
+    question: &str,
+    answer: &str,
+    token: &Option<String>
+) -> Result<serde_json::Value, anyhow::Error> {
+    let query = format!(
+        r#"
+        mutation {{
+            question_assessment(course_slug: "{}", unit_slug: "{}", question_slug: "{}", question: "{}", answer: "{}") {{
+                unit_slug
+                question_slug
+                answer
+                feedback
+                assessment
+                user_id
+            }}
+        }}
+    "#,
+        course_slug,
+        unit_slug,
+        question_slug,
+        question,
+        answer,
+    );
+
+    shared::query(&query, &token).await
+}
+
+async fn get_question_assessment(
+    course_slug: &str,
+    unit_slug: &str,
+    question_slug: &str,
+    token: &Option<String>
+) -> Result<serde_json::Value, anyhow::Error> {
+    let query = format!(
+        r#"
+        query {{
+            question_assessment(course_slug: "{}", unit_slug: "{}", question_slug: "{}") {{
+                unit_slug
+                question_slug
+                answer
+                feedback
+                assessment
+                user_id
+            }}
+        }}
+    "#,
+        course_slug,
+        unit_slug,
+        question_slug,
+    );
+
+    shared::query(&query, &token).await
 }
