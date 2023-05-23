b4fcd3da3110d221cea11656c767b2e336b1d381
Albert <albert@framework.tools>
2023-05-23 17:36:47 +0930

    Enable migration script

--- migrate.sh
+++ migrate.sh
@@ -3,8 +3,6 @@
 PSQLDEF_URL_LINUX=https://github.com/k0kubun/sqldef/releases/download/v0.15.26/psqldef_linux_amd64.tar.gz
 PSQLDEF_URL_MACOS=https://github.com/k0kubun/sqldef/releases/download/v0.15.26/psqldef_darwin_arm64.zip
 
-# print arch
-
 if [[ "$(uname)" == "Darwin" && "$(uname -m)" == "arm64" ]]; then
     PSQLDEF_URL=$PSQLDEF_URL_MACOS
 elif [[ "$(uname)" == "Linux" && "$(uname -m)" == "x86_64" ]]; then
@@ -26,14 +24,13 @@ fi
 # load env files
 set -o allexport; source .env; set +o allexport
 
-# make sure they're all set
-for var_name in PG_HOST PG_USER PG_DATABASE PG_PASSWORD; do
-    if [ -z "${!var_name}" ]; then
-        echo "ERROR: $var_name is not defined"; exit 1
-    fi
-done
+PG_USER=$(echo $DATABASE_URL | cut -d'/' -f3 | cut -d':' -f1)
+PG_PASSWORD=$(echo $DATABASE_URL | cut -d'/' -f3 | cut -d':' -f2 | cut -d'@' -f1)
+PG_HOST=$(echo $DATABASE_URL | cut -d'/' -f3 | cut -d'@' -f2 | cut -d'/' -f1)
+PG_DATABASE=$(echo $DATABASE_URL | cut -d'/' -f4 | cut -d'?' -f1)
 
-# generate schema.sql: ./psqldef -U $PG_USER -W $PG_PASSWORD -h $PG_HOST $PG_DATABASE --export > schema.sql
+# generate schema.sql:
+# ./psqldef -U $PG_USER -W $PG_PASSWORD -h $PG_HOST $PG_DATABASE --export > schema.sql
 
 # run migration
 ./psqldef -U $PG_USER -W $PG_PASSWORD -h $PG_HOST $PG_DATABASE < schema.sql
