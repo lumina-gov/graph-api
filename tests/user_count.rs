34698b8537c24e17f27c8bb11571f3ca40823dbd
Albert <albert@lumina.earth>
2023-06-05 21:54:00 +0930

    Use cargo compiler

--- tests/user_count.rs
+++ tests/user_count.rs
@@ -19,3 +19,23 @@ async fn test_user_count() -> Result<(), anyhow::Error> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn can_get_user_count_by_interval() -> Result<(), anyhow::Error> {
+    let shared_app = shared::SharedApp::init().await;
+
+    let res = shared_app
+        .query(
+            "
+        query {
+            user_count_by_interval(interval: 1, count: 12)
+        }
+    ",
+            &None,
+        )
+        .await?;
+
+    assert!(res["data"]["user_count_by_interval"].is_array());
+
+    Ok(())
+}
